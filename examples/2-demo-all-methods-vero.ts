42f3d8a2ec546c476053d37179d02a5f66e9c24d
Tiago Ribeiro de Barros <90200916+tiagordebarros@users.noreply.github.com>
2026-02-14 17:37:49 +0000

    chore(examples): use logger instead of console

--- examples/2-demo-all-methods-vero.ts
+++ examples/2-demo-all-methods-vero.ts
@@ -3,7 +3,7 @@ import { logger } from "../src/mod.ts";
 const traceExample = () => {
   function foo() {
     function bar() {
-      console.trace();
+      logger.trace();
     }
     bar();
   }
@@ -11,9 +11,7 @@ const traceExample = () => {
   foo();
 };
 
-// TODO: create special Vero header method to display header with style
-
-console.clear(); // TODO: create new Vero method to clear console with style
+logger.clear();
 logger.hr();
 logger.h1("Starting demo of all Vero methods...");
 logger.log("Log example");
@@ -21,29 +19,30 @@ logger.info("Info example");
 logger.warn("Warning example");
 logger.error("Error example");
 logger.debug("Debug example");
+logger.success("Success example");
 logger.table([{ name: "Alice", age: 30 }, { name: "Bob", age: 25 }]);
 logger.time("Timer example");
 logger.timeEnd("Timer example");
 logger.time("answer time");
 alert("Click to continue");
-console.timeLog("answer time"); // TODO: create Vero method for timeLog
+logger.timeLog("answer time");
 alert("Do a bunch of other stuff…");
 logger.timeEnd("answer time");
-console.assert(1 === 1, "This will not log");
-console.assert((1 as number) === 2, "This will log an assertion error");
-console.count("Count example");
-console.count("Count example");
+logger.assert(1 === 1, "This will not log");
+logger.assert((1 as number) === 2, "This will log an assertion error");
+logger.count("Count example");
+logger.count("Count example");
 logger.log("Count reset");
-console.countReset("Count example");
-console.count("Count example");
-console.group("Group example");
+logger.countReset("Count example");
+logger.count("Count example");
+logger.group("Group example");
 logger.log("Inside group");
-console.groupEnd();
-console.groupCollapsed("Collapsed group example");
+logger.groupEnd();
+logger.groupCollapsed("Collapsed group example");
 logger.log("Inside collapsed group");
-console.groupEnd();
-console.dir({ name: "Charlie", age: 35 });
-console.dirxml({ id: 1, name: "Valid example", type: "dirxml" });
-console.trace(traceExample);
+logger.groupEnd();
+logger.dir({ name: "Charlie", age: 35 });
+logger.dirxml({ id: 1, name: "Valid example", type: "dirxml" });
+logger.trace(traceExample);
 logger.log("End of all Vero methods demo");
 logger.hr();
